bc4980ac16a8274f1eb7abf6a8fdbce9d1eef8fa
Mitch Lembke <mitchell.lembke@gmail.com>
2018-04-03 13:24:37 -0600

    All routes working!

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var minidiscsRouter = require('./routes/minidiscs')
 
 var app = express();
 
@@ -18,9 +19,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.disable('x-powered-by')
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/minidiscs', minidiscsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- seeds/001_minidiscs.js
+++ seeds/001_minidiscs.js
@@ -3,14 +3,14 @@ exports.seed = function(knex, Promise) {
 return knex('minidiscs').del()
  .then(function() {
    // Inserts seed entries
-   return knex('minidiscs').insert([
+   return knex('tablename').insert([
      {id: 1, title: 'Black Hole Sun', artist: 'Soundgarden', genre: 'Rock', description: 'Wont ya come', cover_url: 'soundgarden.jpg'},
-     {id: 2, title: 'VS', artist: 'Pearl Jam', genre: 'Rock', description: 'Wonderful, captivating, amazing', cover_url: 'https://upload.wikimedia.org/wikipedia/en/f/f9/PearlJam-Vs.jpg'},
-     {id: 3, title: 'Exciter', artist: 'Depeche Mode', genre: 'Alternative Goth Pop Punk Rock Core', description: 'Post-consumer society blanketed in a tasty malaise of honey and sadness', cover_url: 'https://upload.wikimedia.org/wikipedia/en/b/b3/Depeche_Mode_-_Exciter.png'}
+     {id: 2, title: 'VS', artist: 'Pearl Jam', genre: 'Rock', description: 'Wonderful', cover_url: 'pearljam.jpg'},
+     {id: 3, title: 'Exciter', artist: 'Depeche Mode', genre: 'Alternative', description: 'Sounds like a post-consumer socierty blanketed in a tastey malaise of honey and sadness', cover_url: 'stuff.png'}
    ])
-   .then(function() {
+   .then(() => {
      // Moves id column (PK) auto-incrementer to correct value after inserts
-     return knex.raw("SELECT setval('minidiscs_id_seq', (SELECT MAX(id) FROM minidiscs));")
+     return knex.raw("SELECT setval('minidiscs_id_seq', (SELECT MAX(id) FROM tablename))")
    })
  })
 }
